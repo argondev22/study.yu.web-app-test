47d86a5fbab241540e57f2c2a850425190bf864f
argondev22 <hyoga.js@gmail.com>
2025-10-01 13:29:18 +0900

    feat: matcher_func.test.tsを新規作成し、数値、文字列、真偽値、配列、オブジェクトのテストケースを追加しました。

---
+++ src/section2/matcher_func.test.ts
@@ -0,0 +1,25 @@
+// toBe
+it("数値のテスト", () => {
+  expect(1 + 2).toBe(3);
+});
+
+it("文字列のテスト", () => {
+  expect("Hello, World!").toEqual("Hello, World!");
+});
+
+it("真偽値のテスト", () => {
+  expect(true).toBe(true);
+});
+
+// toEqual
+it("配列のテスト", () => {
+  const arr1 = [1, 2, 3];
+  const arr2 = [1, 2, 3];
+  expect(arr1).toEqual(arr2);
+});
+
+it("オブジェクトのテスト", () => {
+  const obj1 = { a: 1, b: 2 };
+  const obj2 = { a: 1, b: 2 };
+  expect(obj1).toEqual(obj2);
+});
