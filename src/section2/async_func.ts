d1cfc9cbb7ab6e282534b090bc7ec332f9aefee6
argondev22 <hyoga.js@gmail.com>
2025-10-01 13:51:22 +0900

    feat: delay関数を新規作成し、指定された時間後にメッセージを返す機能を実装しました。また、時間がマイナスの場合にエラーをスローするテストケースを追加しました。

---
+++ src/section2/async_func.ts
@@ -0,0 +1,11 @@
+export function delay(message: string, time: number): Promise<string> {
+  return new Promise((resolve, reject) => {
+    if (time >= 0) {
+      return setTimeout(() => {
+        resolve(message);
+      }, time);
+    } else {
+      reject(new Error("timeは0以上である必要があります"));
+    }
+  });
+}
