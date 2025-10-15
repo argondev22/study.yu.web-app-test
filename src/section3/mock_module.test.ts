95e65c07ab9f1fd2fd38732067a08284b19d1e51
argondev22 <hyoga.js@gmail.com>
2025-10-15 22:17:03 +0900

    feat: モック化されたfsライブラリの設定を明確にし、readFileSyncメソッドの戻り値を指定するテストケースを更新しました。

--- src/section3/mock_module.test.ts
+++ src/section3/mock_module.test.ts
@@ -2,9 +2,9 @@ import fs from "fs";
 import { readFile } from "./mock_module";
 
 // モックの設定はトップレベルで記述
-jest.mock("fs");
-const mockFs = jest.mocked(fs);
-mockFs.readFileSync.mockReturnValue("dummy data");
+jest.mock("fs"); // モック化の設定（これによって、fsライブラリがモック化される。モック化後、fsライブラリのメソッドは全て空の関数になる）
+const mockFs = jest.mocked(fs); // TSで記述する場合のみ必要な記述。コンパイラに型を伝えるためのもの。本質的にモック化とは無関係であるため注意
+mockFs.readFileSync.mockReturnValue("dummy data"); // メソッドの挙動を指定
 
 it("readFileがデータを返却する", () => {
   const result = readFile("path/dummy");
