a0daa5beb6962931e6ca0d642083854106ab0eef
argondev22 <hyoga.js@gmail.com>
2025-10-15 22:36:37 +0900

    feat: enhance Users test suite by adding additional test cases and refining axios mock behavior

--- src/section3/practice.test.ts
+++ src/section3/practice.test.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 import Users from "./practice";
 
+jest.mock("axios");
+const mockedAxios = jest.mocked(axios);
+
+// My Answer
 const users = [
   {
     name: "Taro Yamada",
@@ -11,15 +15,38 @@ const users = [
     age: 25
   }
 ]
-
-jest.mock("axios");
-const mockedAxios = jest.mocked(axios);
 mockedAxios.get.mockResolvedValue({
   data: users,
 });
 
-it("", async () => {
+it("ユーザーを取得できる", async () => {
   const result = await Users.all();
   expect(result).toBe(users);
   expect(axios.get).toHaveBeenCalledTimes(1);
+})
+
+// Teacher's Answer
+describe("Users", () => {
+  beforeEach(() => {
+    mockedAxios.get.mockClear();
+  })
+
+  it("ユーザーを取得できる", async () => {
+    const users = [
+      {
+        name: "Taro",
+      },
+      {
+        name: "Hanako",
+      },
+    ];
+    const resp = {
+      data: users,
+    };
+    mockedAxios.get.mockResolvedValue(resp);
+
+    const result = await Users.all();
+    expect(result).toEqual(users);
+    expect(mockedAxios.get).toHaveBeenCalledWith("/users.json");
+  })
 })
