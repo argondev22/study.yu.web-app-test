08abed5d3f624adb6e91d44ba6753ea2f06ddd15
argondev22 <hyoga.js@gmail.com>
2025-11-01 10:08:28 +0900

    feat: replace AsyncComponent with UserSearch in App component for improved user functionality

--- src/section4/src/App.tsx
+++ src/section4/src/App.tsx
@@ -1,13 +1,15 @@
 // import Button from "./components/Button"
 // import Form from "./components/Form"
-import AsyncComponent from "./components/AsyncComponent"
+// import AsyncComponent from "./components/AsyncComponent"
+import { UserSearch } from "./components/UserSearch"
 
 function App() {
   return (
     <>
       {/* <Button label="ボタン" onClick={() => alert("clicked")} /> */}
       {/* <Form /> */}
-      <AsyncComponent />
+      {/* <AsyncComponent /> */}
+      <UserSearch />
     </>
   )
 }
